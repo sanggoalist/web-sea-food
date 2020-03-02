c1d780e125670527d9b55b2ed4355349b86899d7
sanggoalist <s.tran@goalist.co.jp>
2020-03-02 16:44:11 +0700

    Firebase config

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,12 +4,14 @@ import firestore from 'firebase/firestore'
 const settings = {timestampsInSnapshots: true};
 
 const config = {
-  apiKey: "AIzaSyBJJVAR2_S6rTnmuAC_anBN6NV4OAdnd0I",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  databaseURL: "YOUR_DATABASE_URL",
-  projectId: "web-sea-food",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_ID"
+    apiKey: "AIzaSyBJJVAR2_S6rTnmuAC_anBN6NV4OAdnd0I",
+    authDomain: "web-sea-food.firebaseapp.com",
+    databaseURL: "https://web-sea-food.firebaseio.com",
+    projectId: "web-sea-food",
+    storageBucket: "web-sea-food.appspot.com",
+    messagingSenderId: "351172669160",
+    appId: "1:351172669160:web:0977cc2504393e8eb6235d",
+    measurementId: "G-T1M0SRF3MZ"
 };
 firebase.initializeApp(config);
 
